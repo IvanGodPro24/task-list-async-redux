f01cc8a12a58b8df2dbb518ebb3158e3b4858210
IvanGodPro24 <vanyanep@gmail.com>
2025-01-28 22:29:22 +0200

    Add redux state management

--- src/components/TaskForm/TaskForm.jsx
+++ src/components/TaskForm/TaskForm.jsx
@@ -1,10 +1,24 @@
 import { Button } from "../Button/Button";
 import css from "./TaskForm.module.css";
 
+import { useDispatch } from "react-redux";
+import { addTask } from "../../redux/actions";
+
 export const TaskForm = () => {
+  const dispatch = useDispatch();
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const form = event.target;
+
+    dispatch(
+      addTask({
+        id: crypto.randomUUID(),
+        text: form.elements.text.value,
+        completed: false,
+      })
+    );
+
     form.reset();
   };
 
