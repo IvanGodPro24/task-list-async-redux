0041c5b0bba41786a642febf4bc50241722eb048
IvanGodPro24 <vanyanep@gmail.com>
2025-01-30 10:52:12 +0200

    Add persistor

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,13 +2,42 @@ import { configureStore } from "@reduxjs/toolkit";
 import taskReducer from "./tasksSlice";
 import filterReducer from "./filtersSlice";
 
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, taskReducer);
+
 export const store = configureStore({
   //   reducer: rootReducer,
   reducer: {
-    tasks: taskReducer,
+    tasks: persistedReducer,
     filters: filterReducer,
   },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
 
 // const initialState = {
 //   tasks: {
--- src/redux/tasksSlice.js
+++ src/redux/tasksSlice.js
@@ -4,13 +4,7 @@ const slice = createSlice({
   name: "tasks",
 
   initialState: {
-    items: [
-      { id: 0, text: "Learn HTML and CSS", completed: true },
-      { id: 1, text: "Get good at JavaScript", completed: true },
-      { id: 2, text: "Master React", completed: false },
-      { id: 3, text: "Discover Redux", completed: false },
-      { id: 4, text: "Build amazing apps", completed: false },
-    ],
+    items: [],
   },
 
   reducers: {
@@ -24,7 +18,7 @@ const slice = createSlice({
     },
 
     deleteTask(state, action) {
-      state.items.filter((task) => task.id !== action.payload);
+      state.items = state.items.filter((task) => task.id !== action.payload);
 
       //   return {
       //     ...state,
